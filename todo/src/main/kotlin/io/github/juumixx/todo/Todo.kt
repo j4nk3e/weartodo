7a9db701241093a16b686528644d516f9f4be7d0
juumixx <juumixx@gmail.com>
2017-06-05 19:00:32 +0200

    todo.txt parser module

---
+++ todo/src/main/kotlin/io/github/juumixx/todo/Todo.kt
@@ -0,0 +1,118 @@
+package io.github.juumixx.todo
+
+import org.joda.time.LocalDateTime
+import org.joda.time.format.DateTimeFormat
+
+class Todo(val txt: String) {
+    val tasks = ArrayList<Task>()
+
+    init {
+        tasks.addAll(read())
+    }
+
+    fun read(): MutableList<Task> = txt.split("\n").filter(String::isNotBlank).map { Task(it) }.toMutableList()
+}
+
+data class Task(val txt: String) {
+    companion object {
+        private val COMPLETED = Regex("^x\\s+(.+)")
+        private val PRIORITY = Regex("^\\(([A-Z])\\)\\s+(.+)")
+        private val COMPLETION_DATE = Regex("^x\\s+([0-9]{2}-[0-9]{2}-[0-9]{4})\\s+([0-9]{2}-[0-9]{2}-[0-9]{4})\\s+(.+)")
+        private val CREATION_DATE = Regex("^(\\([A-Z]\\)\\s+)?([0-9]{2}-[0-9]{2}-[0-9]{4})\\s+(.+)")
+        private val CREATION_DATE_COMPLETED = Regex("^x\\s+([0-9]{2}-[0-9]{2}-[0-9]{4})\\s+(.+)")
+        private val PROJECT = Regex("\\+(\\S+)")
+        private val CONTEXT = Regex("@(\\S+)")
+        private val TAG = Regex("([^\\s:]+):([^\\s:]+)")
+        val DATE_STRING = "dd-MM-yyyy"
+        val DATE_FORMAT = DateTimeFormat.forPattern(DATE_STRING)!!
+    }
+
+    private val trim = txt.trim()
+    val completed = COMPLETED.matches(trim)
+    val priority = PRIORITY.matchEntire(trim)?.groupValues?.get(1)
+    val completionDate: LocalDateTime?
+    val creationDate: LocalDateTime?
+    val content: String
+    val projects = ArrayList<String>()
+    val contexts = ArrayList<String>()
+    val tags = ArrayList<Pair<String, String>>()
+
+    init {
+        if (completed) {
+            val completionMatch = COMPLETION_DATE.matchEntire(trim)
+            if (completionMatch != null) {
+                completionDate = completionMatch.groupValues[1].let { LocalDateTime.parse(it, DATE_FORMAT) }
+                creationDate = completionMatch.groupValues[2].let { LocalDateTime.parse(it, DATE_FORMAT) }
+                content = completionMatch.groupValues[3]
+            } else {
+                val creationMatch = CREATION_DATE_COMPLETED.matchEntire(trim)
+                if (creationMatch != null) {
+                    completionDate = null
+                    creationDate = creationMatch.groupValues[1].let { LocalDateTime.parse(it, DATE_FORMAT) }
+                    content = creationMatch.groupValues[2]
+                } else {
+                    completionDate = null
+                    creationDate = null
+                    content = COMPLETED.matchEntire(trim)!!.groupValues[1]
+                }
+            }
+        } else {
+            completionDate = null
+            val creationMatch = CREATION_DATE.matchEntire(trim)
+            if (creationMatch != null) {
+                creationDate = creationMatch.groupValues[2].let { LocalDateTime.parse(it, DATE_FORMAT) }
+                content = creationMatch.groupValues[3]
+            } else {
+                creationDate = null
+                val priorityMatch = PRIORITY.matchEntire(trim)
+                if (priorityMatch != null) {
+                    content = priorityMatch.groupValues[2]
+                } else {
+                    content = trim
+                }
+            }
+        }
+        content.split(" ").forEach { checkSpecial(it) }
+    }
+
+    override fun toString(): String {
+        val sb = StringBuilder()
+        if (completed) {
+            sb.append("x ")
+        } else if (priority != null) {
+            sb.append("($priority) ")
+        }
+        if (completionDate != null) {
+            sb.append("${completionDate.toString(DATE_FORMAT)} ")
+        }
+        if (creationDate != null) {
+            sb.append("${creationDate.toString(DATE_FORMAT)} ")
+        }
+        sb.append(content)
+        return sb.toString()
+    }
+
+    private fun checkSpecial(word: String): Boolean {
+        if (word.isBlank()) {
+            return false
+        }
+        val projectMatch = PROJECT.matchEntire(word)
+        if (projectMatch != null) {
+            projects.add(projectMatch.groupValues[1])
+            return false
+        }
+        val contextMatch = CONTEXT.matchEntire(word)
+        if (contextMatch != null) {
+            contexts.add(contextMatch.groupValues[1])
+            return false
+        }
+        val tagMatch = TAG.matchEntire(word)
+        if (tagMatch != null) {
+            val key = tagMatch.groupValues[1]
+            val value = tagMatch.groupValues[2]
+            tags.add(Pair(key, value))
+            return false
+        }
+        return true
+    }
+}
---
+++ todo/src/test/kotlin/io/github/juumixx/todo/TodoTest.kt
@@ -0,0 +1,95 @@
+package io.github.juumixx.todo
+
+import io.github.juumixx.todo.Task.Companion.DATE_STRING
+import org.amshove.kluent.shouldBe
+import org.amshove.kluent.shouldEqual
+import org.junit.Test
+
+class TodoTest {
+    @Test
+    fun shouldReadTodoPerLine() {
+        val txt = """task 1
+x task (2) is already done
+(A) 04-05-2003 task x 3 @context
+(B) task 4 @context +project
+X task 5 +project
+
+task 6 is not priority (A) but due:01-02-2003
+x 03-04-2003 04-05-2003 task 7
+(A) task 8 @ctx
+(B) task 9
+(C) 04-05-2003 task 10
+"""
+        val todo = Todo(txt)
+        todo.tasks.size shouldBe 10
+        todo.tasks.joinToString("\n") shouldEqual txt.replace("\n\n", "\n").trim()
+    }
+
+    @Test
+    fun shouldReadCompletedTask() {
+        Task("x task (2) is already done").completed shouldBe true
+        Task("(A) 04-05-2003 task x 3 @context").completed shouldBe false
+        Task("X task 5 +project").completed shouldBe false
+    }
+
+    @Test
+    fun shouldParsePriority() {
+        Task("(A) 04-05-2003 task x 3 @context").priority shouldEqual "A"
+        Task("(B) task 4 @context +project").priority shouldEqual "B"
+        Task("X task 5 +project").priority shouldBe null
+        Task("(A)07-06-2017 no date or priority").priority shouldBe null
+    }
+
+    @Test
+    fun shouldParseCreationDate() {
+        Task("x 06-06-2017 05-06-2017 task 3 @context").creationDate?.toString(DATE_STRING) shouldEqual "05-06-2017"
+        Task("07-06-2017 task was created today").creationDate?.toString(DATE_STRING) shouldEqual "07-06-2017"
+        Task("(A) 06-06-2017 task x 2 +project").creationDate?.toString(DATE_STRING) shouldEqual "06-06-2017"
+        Task("(A)07-06-2017 no date or priority").creationDate shouldBe null
+        Task("is not priority (A) but due:01-02-2003").creationDate shouldBe null
+        Task("x do 01-02-2003 something").creationDate shouldBe null
+    }
+
+    @Test
+    fun shouldParseCompletionDate() {
+        Task("x 06-06-2017 07-06-2017 task was created today").completionDate?.toString(DATE_STRING) shouldEqual "06-06-2017"
+        Task("x 01-06-2017 07-12-2016 task was done +01-02-2003").completionDate?.toString(DATE_STRING) shouldEqual "01-06-2017"
+        Task("x 05-06-2017 task 3 @context").completionDate?.toString(DATE_STRING) shouldBe null
+        Task("(A) 06-06-2017 07-06-2017 task x 2 +project").completionDate shouldBe null
+        Task("(A)07-06-2017 no date or priority").completionDate shouldBe null
+        Task("is not priority (A) but due:01-02-2003").completionDate shouldBe null
+        Task("(B) do 01-02-2003 something").completionDate shouldBe null
+    }
+
+    @Test
+    fun shouldParseProjects() {
+        Task("(A) 04-05-2003 task x 3 @context").projects.size shouldBe 0
+        Task("(B) task 4 @context +project").projects shouldEqual arrayListOf("project")
+        Task("X task 5 +project").projects shouldEqual arrayListOf("project")
+        Task("(A)07-06-2017 no +date or +priority").projects shouldEqual arrayListOf("date", "priority")
+    }
+
+    @Test
+    fun shouldParseContexts() {
+        Task("X task 5 +project").contexts.size shouldBe 0
+        Task("(A) 04-05-2003 task x 3 @context").contexts shouldEqual arrayListOf("context")
+        Task("(B) task 4 @context +project").contexts shouldEqual arrayListOf("context")
+        Task("(A)07-06-2017 no @date or @priority").contexts shouldEqual arrayListOf("date", "priority")
+    }
+
+    @Test
+    fun shouldParseTags() {
+        Task("is not priority (A) but due: 01-02-2003").tags.size shouldBe 0
+        Task("(A) task 8 @ctx").tags.size shouldBe 0
+        Task("01-02-2017 is not priority (A) but due:01-02-2003").tags shouldEqual arrayListOf(Pair("due", "01-02-2003"))
+        Task("any:tag can:be any:where").tags shouldEqual arrayListOf(Pair("any", "tag"), Pair("can", "be"), Pair("any", "where"))
+    }
+
+    @Test
+    fun shouldNotAlterContent() {
+        Task("X task 5 +project").content shouldEqual "X task 5 +project"
+        Task("(A) 04-05-2003 task x 3 @context").content shouldEqual "task x 3 @context"
+        Task("x task 4 @context +project").content shouldEqual "task 4 @context +project"
+        Task("(A)07-06-2017 no @date or @priority").content shouldEqual "(A)07-06-2017 no @date or @priority"
+    }
+}
